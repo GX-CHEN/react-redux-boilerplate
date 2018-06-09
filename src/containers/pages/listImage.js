970521623abc85685d6f345099562c06d950b806
gongxia.chen <chengongxia1990@gmail.com>
2018-06-09 00:17:35 -0400

    add some more comments

--- src/containers/pages/listImage.js
+++ src/containers/pages/listImage.js
@@ -8,7 +8,7 @@ import PropTypes from 'prop-types';
 import { isEqual } from 'lodash';
 
 class ListImage extends React.Component {
-  /**@constructor 
+  /**@constructor
    * @param {Object} props
    * @param {Object} props.location - location is from redux-react-router, which maintains current page and variables send to the page
    * @param {Function} props.changePage - changePage is a function which can navigation from a page to another
@@ -25,10 +25,22 @@ class ListImage extends React.Component {
     message.success('Land on listing page');
   }
 
+  /**
+   * dispatch the searchImage action and return a list of image
+   */
   listImage = () => {
     this.props.searchImage();
   };
 
+  /**
+   * getDerivedStateFromProps will be called everytime before render (it is new React lifecyle function to replace the old componentWillRecieveProps)
+   * So in order to avoid too frequent render, we need to compare the data then decide do we need to re-render
+   * @param {Object} nextProps
+   * @param {Object} prevState
+   * @param {Array} nextProps.imageList - imageList from nextProps (new one)
+   * @param {Array} prevState.imageList - imageList which now existed in state
+   * @returns {state} - will return new imageList if there's change
+   */
   static getDerivedStateFromProps(nextProps, prevState) {
     if (isEqual(nextProps.imageList, prevState.imageList)) {
       return null;
@@ -39,6 +51,10 @@ class ListImage extends React.Component {
     }
   }
 
+  /**
+   * @param {string} imageUrl - url of image which pass to next page
+   * @param {string} imageTitle - title of image which pass to next page
+   */
   navigateToImageView = (imageUrl, imageTitle) => {
     const { changePage } = this.props;
     changePage('viewImage', { imageUrl, imageTitle });
--- src/model/apiService.js
+++ src/model/apiService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 /**
  * place all the service calls in this model folder.
- * If it's a small app (within 10 calls), it's OK to put them in a single file. If there are many calls, better to place in seprate files
+ * If it's a small app (within 10 calls), it's OK to put them in a single file. If there are many calls, better to place in separate files
  */
 
 const BASE_URL = 'http://api.giphy.com/v1/gifs/trending';
@@ -10,6 +10,7 @@ const API_KEY = 'f30RsH8N2igFVgyuE3KBH3t0RjzzkWne';
 
 /**
  * search trending do not require any passed in argument, it simply get the lastest trending images from giphy
+ * @returns {Array} - An array of giphy gif images json data will be returned from the API call
  */
 export function searchImageService() {
   return axios.get(`${BASE_URL}?api_key=${API_KEY}&limit=10`).then(function(res) {
